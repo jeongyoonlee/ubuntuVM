5ba0f1ad79825c284059125201fba84957108e81
John-Mark Agosta <john-mark.agosta@gmail.com>
2018-09-28 15:53:03 -0700

    fix test for DSVM test

--- min_install_amlpf_linux.sh
+++ min_install_amlpf_linux.sh
@@ -89,7 +89,7 @@ fi
 
 conda_version_check
 
-if $VM_OFFER =~ ".*data-science.*"; then
+if [ $VM_OFFER == "linux-data-science-vm-ubuntu" ]; then    # god help us if the DSVM product name is changed. 
     CONDA_BASE_ENV='py35'
 else
     # Add things needed in the DSVM
