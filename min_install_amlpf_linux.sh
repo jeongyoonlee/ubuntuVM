9f26a759c30d8f0482af1b6f436b421db462f5a5
John-Mark Agosta <john-mark.agosta@gmail.com>
2018-10-01 12:08:10 -0700

    minimum min install

--- min_install_amlpf_linux.sh
+++ min_install_amlpf_linux.sh
@@ -90,17 +90,18 @@ else
     printf "$(env | grep VM) \n"
 fi
 
-conda_version_check
+printf ">>> VM_OFFER:  ${VM_OFFER}"
+# conda_version_check
 
 if [ -n "${VM_OFFER}" ]; then
     CONDA_BASE_ENV='py35'
 else
     # Add things needed in the DSVM
    printf "We are on plain ubuntu, install mono\n"
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-    echo "deb http://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
-    sudo apt-get update
-    sudo apt-get -y install mono-complete
+    # sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    # echo "deb http://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+    # sudo apt-get update
+    # sudo apt-get -y install mono-complete
     OS=${UBUNTU}
     printf "Create conda env $CONDA_BASE_ENV\n"
     # Vanilla ubuntu conda install was done in conda_version_check()
