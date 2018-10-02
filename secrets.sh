f7f92911c88369297867c967766480781523c4a6
John-Mark Agosta <john-mark.agosta@gmail.com>
2018-10-02 15:35:11 -0700

    new attributes

--- secrets.sh
+++ secrets.sh
@@ -1,2 +1,3 @@
+# declare resourceGroupName="" Leave this blank to force creation of a new resource group
 declare resourceGroupLocation="West US 2"
 declare subscriptionId="XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
